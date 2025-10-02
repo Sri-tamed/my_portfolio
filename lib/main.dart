bd3ff3c5c163d57a6ddcf0c2a71df4c48048a4ae
Sri-tamed <sritamachatterjee422@gmail.com>
2025-10-03 00:45:37 +0530

    changed name

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppTheme.darkTheme,
-      title: 'Akshit Madan👋 | Developer & Content Creator',
+      title: 'Sritama | Developer & Content Creator',
       debugShowCheckedModeBanner: false,
       home: const HomePage(),
     );
